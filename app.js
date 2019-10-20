70ea161cb156af5034f8f7c2034e38ef3de5c20a
Patryk Kurzeja <patryk.kurzeja@monterail.com>
2019-10-20 19:40:03 +0200

    fixes in styles and others

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ window.addEventListener('load', e => {
   if ('serviceWorker' in navigator) {
     try {
       navigator.serviceWorker.register('serviceWorker.js')
-      console.log('SW registered');
     } catch {
-console.log('Sw FAILED');
+      console.log('Sw FAILED')
     }
   }
 })
